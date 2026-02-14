dc015cbf6c0d023e24303c84c7c145511d0bdb30
Yvan <yvandumas05@gmail.com>
2026-02-14 16:21:07 +0100

    Modified the About parapgraph

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,4 +1,4 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation, Trans } from "react-i18next";
 
 import chopperImg from "../assets/images/Chopper.png";
 
@@ -9,11 +9,15 @@ export default function About() {
         <section id="about">
             <h2 className="text-section-heading font-bold max-w-full mx-auto px-8 tracking-tight">{t('about.title')}</h2>
             <div className="flex flex-col items-center center md:flex-row px-10 md:pl-20 gap-10">
-                <p className="mt-10 md:mt-15 whitespace-pre-line">{t('about.description')}</p>
-                <img src={chopperImg} alt="3D modeling of Tony Tony Chopper from One Piece" className="flex-none w-[20vw] min-w-37.5 max-w-125 object-contain aspect-auto drop-shadow-2xl animate-float"/>
+                <p className="mt-10 md:mt-15 whitespace-pre-line">
+                    <Trans
+                    i18nKey="about.description"
+                    components={{ b: <strong className="font-bold" /> }}
+                    />
+                </p>
+                <img src={chopperImg} alt="3D modeling of Tony Tony Chopper from One Piece" className="flex-none w-[20vw] min-w-37.5 max-w-125 object-contain aspect-auto drop-shadow-2xl animate-float" />
             </div>
-            <p><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br/></p>
+            <p><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /></p>
         </section>
     );
 }
