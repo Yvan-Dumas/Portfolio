22d8d4b353bcb307c179fa99ddef5227ed17de68
Yvan <yvandumas05@gmail.com>
2026-02-14 16:03:22 +0100

    Added Chopper image to About section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,18 +1,19 @@
 import { useTranslation } from "react-i18next";
 
+import chopperImg from "../assets/images/Chopper.png";
+
 export default function About() {
     const { t } = useTranslation();
 
     return (
         <section id="about">
             <h2 className="text-section-heading font-bold max-w-full mx-auto px-8 tracking-tight">{t('about.title')}</h2>
-            <p className="mt-10 md:mt-15 px-10 md:px-20">{t('about.description')}
-                <br /><br /><br /><br /><br />
-                <br /><br /><br /><br /><br />
-                <br /><br /><br /><br /><br />
-                <br /><br /><br /><br /><br />
-                <br /><br /><br /><br /><br />
-            </p>
+            <div className="flex flex-col items-center center md:flex-row px-10 md:pl-20 gap-10">
+                <p className="mt-10 md:mt-15 whitespace-pre-line">{t('about.description')}</p>
+                <img src={chopperImg} alt="3D modeling of Tony Tony Chopper from One Piece" className="flex-none w-[20vw] min-w-[150px] max-w-[500px] object-contain aspect-auto drop-shadow-2xl animate-float"/>
+            </div>
+            <p><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br/></p>
         </section>
     );
 }
