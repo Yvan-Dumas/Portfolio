e385d4fe3df9c9a3fcac298bf57e47b10977bf28
Yvan <yvandumas05@gmail.com>
2026-02-14 16:05:44 +0100

    Fix property for the min and max w for the Chopper's image

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
             <h2 className="text-section-heading font-bold max-w-full mx-auto px-8 tracking-tight">{t('about.title')}</h2>
             <div className="flex flex-col items-center center md:flex-row px-10 md:pl-20 gap-10">
                 <p className="mt-10 md:mt-15 whitespace-pre-line">{t('about.description')}</p>
-                <img src={chopperImg} alt="3D modeling of Tony Tony Chopper from One Piece" className="flex-none w-[20vw] min-w-[150px] max-w-[500px] object-contain aspect-auto drop-shadow-2xl animate-float"/>
+                <img src={chopperImg} alt="3D modeling of Tony Tony Chopper from One Piece" className="flex-none w-[20vw] min-w-37.5 max-w-125 object-contain aspect-auto drop-shadow-2xl animate-float"/>
             </div>
             <p><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
             <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br/></p>
