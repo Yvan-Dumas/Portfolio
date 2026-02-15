a917950ebff3da49d48a82becb06fe1f28b95d1b
Yvan <yvandumas05@gmail.com>
2026-02-15 12:11:46 +0100

    modified image size and gap

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -11,8 +11,8 @@ export default function About() {
             <div className="flex flex-col items-center center md:flex-row px-10 md:pl-20 gap-10 md:gap-20">
                 <p className="mt-10 md:mt-15 whitespace-pre-line">
                     <Trans
-                    i18nKey="about.description"
-                    components={{ b: <strong className="font-bold" /> }}
+                        i18nKey="about.description"
+                        components={{ b: <strong className="font-bold" /> }}
                     />
                 </p>
                 <img src={chopperImg} alt="3D modeling of Tony Tony Chopper from One Piece" className="flex-none w-[18vw] min-w-37.5 max-w-125 object-contain aspect-auto drop-shadow-2xl animate-float" />
