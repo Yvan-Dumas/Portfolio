9a8e639b1e520f9b2519c5552cdd03c942f6d820
Yvan <yvandumas05@gmail.com>
2026-02-18 17:02:41 +0100

    Added language switch functionnality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import gsap from "gsap";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-import ScrollButton from "./ScrollButton";
+import ActionButton from "./ActionButton";
+
+import { useTranslation } from "react-i18next";
+import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(ScrollToPlugin);
 
@@ -11,17 +14,40 @@ export default function Navbar() {
         { name: 'Skills', href: "skills" },
         { name: 'Projects', href: 'projects' },
         { name: 'Journey', href: 'journey' },
-        { name : 'Contact', href : 'contact'}
+        { name: 'Contact', href: 'contact' }
     ];
 
+    // Smooth scroll with GSAP
     const handleClick = (e, id) => {
         e.preventDefault();
         gsap.to(window, { duration: 1, scrollTo: { y: `#${id}`, offsetY: 0 }, ease: "power3.inOut" });
     };
 
 
+    const { i18n } = useTranslation();
+
+    // Change language
+    const toggleLanguage = () => {
+        const newLanguage = i18n.language === 'fr' ? 'en' : 'fr';
+        gsap.to(".smooth-lang", {
+            opacity: 0,
+            duration: 0.4,
+            onComplete: () => {
+                // 2. On change la langue quand c'est invisible
+                i18n.changeLanguage(newLanguage);
+
+                // 3. On fait réapparaître le contenu avec le nouveau texte
+                gsap.to(".smooth-lang", {
+                    opacity: 1,
+                    duration: 0.4,
+                    ease: "power2.out"
+                });
+            }
+        });        
+    }
+
     return (
-        <nav className="md:fixed top-0 z-50 w-full flex justify-between items-start max-w-full mx-auto px-6 py-6 pointer-events-none selection:bg-hover">
+        <nav className="smooth-lang md:fixed top-0 z-50 w-full flex justify-between items-start max-w-full mx-auto px-6 py-6 pointer-events-none selection:bg-hover">
 
             {/* left part: navigation */}
             <div className="flex flex-col items-start leading-tight pointer-events-auto">
@@ -31,7 +57,7 @@ export default function Navbar() {
                 <ul className="flex flex-col ml-8">
                     {navLinks.map((link) => (
                         <li key={link.name}>
-                            <a onClick={ (e) => handleClick(e, link.href) } href={`#${link.href}`} className="font-extralight hover:underline transition-all text-sm">
+                            <a onClick={(e) => handleClick(e, link.href)} href={`#${link.href}`} className="font-extralight hover:underline transition-all text-sm">
                                 / {link.name}
                             </a>
                         </li>
@@ -41,7 +67,9 @@ export default function Navbar() {
 
             {/* right part: language btn */}
             <div className="pointer-events-auto">
-                <ScrollButton content={"> Language"}/>
+                <ActionButton
+                    handleClick={() => toggleLanguage()}
+                    content={`Language : ${i18n.language === 'fr' ? 'FR (WIP)' : 'EN'}`} />
             </div>
         </nav>
     );
