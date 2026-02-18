9d13831a72534e857190cd2a9ccfee3bd84eb9ee
Yvan <yvandumas05@gmail.com>
2026-02-18 12:36:45 +0100

    Updated button to component for the button Contact me

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,33 @@
-export default function Button({ content }) {
+import gsap from "gsap";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
-    return (
-        <button className="
+gsap.registerPlugin(ScrollToPlugin);
+
+export default function Button({ content, href }) {
+    // Buttons style
+    const className=`
             border-2 border-text font-bold rounded-full px-2 py-0.5 md:px-4 md:py-1 
             hover:bg-hover hover:shadow-[0_8px_30px_rgb(0,0,0,0.12)] active:scale-95 transition-all duration-500 ease-out
-            cursor-pointer overflow-hidden pointer-event-auto
-        ">
-            { content }
+            cursor-pointer overflow-hidden
+        `
+
+    // Smooth scroll with GSAP
+    const handleClick = () => {
+        console.log("a");
+        gsap.to(window, { duration: 1, scrollTo: { y: href, offsetY: 0 }, ease: "power3.inOut" });
+    };
+
+    if (href) {
+        return (
+            <a onClick={() => handleClick()}  className={className}>
+                {content}
+            </a>
+        );
+    }
+
+    return (
+        <button className={className}>
+            {content}
         </button>
     );
 }
