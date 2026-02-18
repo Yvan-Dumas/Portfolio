9136316c7d8e7f79908ecda3a5f7e86a9d450a4b
Yvan <yvandumas05@gmail.com>
2026-02-18 14:33:10 +0100

    added privacy property

--- src/components/BentoItem.jsx
+++ src/components/BentoItem.jsx
@@ -8,7 +8,7 @@ export default function BentoItem({title, image, className, link}) {
         h-full w-auto
         ${className}
         `}>
-            <a href={link} target="_blank">
+            <a href={link} target="_blank" rel="noreferrer">
                 <img src={image} alt={`${title} project.`} className="w-full h-full object-cover object-center"/>
             </a>
         </div>
