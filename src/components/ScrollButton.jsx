964202eab5dd05d3162e1bc1595c3f915c2f9c17
Yvan <yvandumas05@gmail.com>
2026-02-18 16:12:15 +0100

    Refactored buttons components

---
+++ src/components/ScrollButton.jsx
@@ -0,0 +1,18 @@
+import gsap from "gsap";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+gsap.registerPlugin(ScrollToPlugin);
+
+export default function Button({ content, href }) {
+
+    // Smooth scroll with GSAP
+    const handleClick = () => {
+        gsap.to(window, { duration: 1, scrollTo: { y: href, offsetY: 0 }, ease: "power3.inOut" });
+    };
+    
+    return (
+        <a onClick={() => handleClick()} className="btn-style">
+            {content}
+        </a>
+    );
+}
