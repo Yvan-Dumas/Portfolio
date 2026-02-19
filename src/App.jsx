6448e5fb49998880e7db9c4ca21f91ecc4cfef94
Yvan <yvandumas05@gmail.com>
2026-02-19 22:12:24 +0100

    Added custom cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import gsap from 'gsap';
+import AnimatedCursor from "react-animated-cursor";
+
 import { useGSAP } from '@gsap/react';
 import { ReactLenis } from 'lenis/react';
 import { useRef } from 'react';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 
 import Navbar from './components/Navbar';
 import ScrollDots from './components/ScrollDots';
@@ -30,24 +33,51 @@ export default function App() {
 	}, []);
 
 	return (
-		<ReactLenis root options={{
-			autoRaf: false,
-			duration: 1.5,
-			lerp: 0.08,
-			wheelMultiplier: 1,
-			infinite: false,
-			touchMultiplier: 2,
-		}} ref={lenisRef}>
-			<Navbar />
-			<ScrollDots />
-			<main className='smooth-lang selection:bg-hover md:px-36'>
-				<Hero />
-				<About />
-				<Skills />
-				<Projects />
-				<Education />
-				<Contact />
-			</main>
-		</ReactLenis>
+		<>
+			<AnimatedCursor
+				innerSize={8}
+                outerSize={35}
+                innerScale={0.8}
+                outerScale={1.5}
+                outerAlpha={0}
+				trailingSpeed={8}
+                hasBlendMode={true}
+                innerStyle={{
+                    backgroundColor: 'var(--color-text)',
+                    zIndex: 9999
+                }}
+                outerStyle={{
+                    border: '1px solid var(--color-text)',
+                    zIndex: 9998
+                }}
+                clickables={[
+                    'a',
+                    'button',
+                    'input',
+                    'select',
+                    'textarea',
+                    '.link',
+                ]}
+			/>
+			<ReactLenis root options={{
+				autoRaf: false,
+				duration: 1.5,
+				lerp: 0.08,
+				wheelMultiplier: 1,
+				infinite: false,
+				touchMultiplier: 2,
+			}} ref={lenisRef}>
+				<Navbar />
+				<ScrollDots />
+				<main className='smooth-lang selection:bg-hover md:px-36'>
+					<Hero />
+					<About />
+					<Skills />
+					<Projects />
+					<Education />
+					<Contact />
+				</main>
+			</ReactLenis>
+		</>
 	);
 }
