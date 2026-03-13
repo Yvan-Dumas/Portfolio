4eebad264b8ddb7c7a2e0f10493fb5e113dbe205
Yvan <yvandumas05@gmail.com>
2026-03-13 21:02:57 +0100

    Add responsive rules

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -3,13 +3,13 @@ export default function SkillCard({ skill }) {
         <div className="flex items-center flex-none 
             border border-text rounded-full 
             px-2 py-0.5
-            w-30
-            md:w-38 h-auto
-            text-sm
-            md:text-base
+            w-27 md:w-33 lg:w-38 h-auto
+            text-xs
+            md:text-sm
+            lg:text-base
             hover:scale-105 transition-all duration-200 ease-in hover:bg-hover hover:shadow-[0_8px_30px_rgb(0,0,0,0.12)]
         ">
-            <img src={skill.icon} alt={`${skill.name} logo`} className="object-contain flex-none h-auto w-8 md:w-10"/>
+            <img src={skill.icon} alt={`${skill.name} logo`} className="object-contain flex-none h-auto w-7 md:w-9 lg:w-11"/>
             <div className="flex-1 flex items-center justify-center overflow-visible text-center">
                 <p className="leading-none">{skill.name}</p>
             </div>
